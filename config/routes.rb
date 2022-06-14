cdeed27ff5cfd997c887efa34c70203f0976f780
Aurelien1997 <tawpo36@gmail.com>
2022-06-14 19:12:14 +0100

    cookies jwt

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
       resources :apartments
     end
   end
-
+  
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             }
+  get '/member-data', to: 'members#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
