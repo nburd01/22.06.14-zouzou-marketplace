3eca233e893f04a15a93a22b898b40c9cdb8d7ee
Niall Burdon <n.burdon@protonmail.com>
2022-06-14 18:56:26 +0200

    devise Editor Nano

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "*"
 
     resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    headers: :any,
+    methods: %i[get post put patch delete options head],
+    expose: %w[Authorization Uid]
   end
 end
+
+
