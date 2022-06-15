51ba94f4db2b44a5fb998d7679c1988a967213fe
Niall Burdon <n.burdon@protonmail.com>
2022-06-15 10:43:02 +0200

    routes adapted

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  # devise_for :users
-  # namespace :api do 
-  #   namespace :v1 do
-  #     resources :apartments
-  #   end
-  # end
-  
   devise_for :users,
-             controllers: {
-               sessions: 'users/sessions',
-               registrations: 'users/registrations'
-             }
-  get '/member-data', to: 'members#show'
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+    get '/member-data', to: 'members#show'
+  namespace :api do 
+    namespace :v1 do
+      resources :apartments
+    end
+  end
+  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
