01d43d20683f4b7ea9a1bab937e872a12c753ded
Pierre Legoux <pierrelegoux@MacBook-Pro-de-Pierre.local>
2022-06-15 15:06:26 +0200

    update join table

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,2 +1,3 @@
 class Apartment < ApplicationRecord
+    belongs_to :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
+
+
+  has_many :apartments
+
 end
